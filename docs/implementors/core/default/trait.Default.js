13ec518105c29faecebd13c7cb9ec68e5c417e35
Chris Jones <cj@hanscj.com>
2016-09-19 11:39:29 -0400

    Added docs

---
+++ docs/implementors/core/default/trait.Default.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["ceph_rust"] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/default/trait.Default.html' title='core::default::Default'>Default</a> for <a class='struct' href='ceph_rust/struct.Struct_rados_pool_stat_t.html' title='ceph_rust::Struct_rados_pool_stat_t'>Struct_rados_pool_stat_t</a>","impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/default/trait.Default.html' title='core::default::Default'>Default</a> for <a class='struct' href='ceph_rust/struct.Struct_rados_cluster_stat_t.html' title='ceph_rust::Struct_rados_cluster_stat_t'>Struct_rados_cluster_stat_t</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ docs/implementors/core/marker/trait.Copy.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["ceph_rust"] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/marker/trait.Copy.html' title='core::marker::Copy'>Copy</a> for <a class='struct' href='ceph_rust/struct.Struct_rados_pool_stat_t.html' title='ceph_rust::Struct_rados_pool_stat_t'>Struct_rados_pool_stat_t</a>","impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/marker/trait.Copy.html' title='core::marker::Copy'>Copy</a> for <a class='struct' href='ceph_rust/struct.Struct_rados_cluster_stat_t.html' title='ceph_rust::Struct_rados_cluster_stat_t'>Struct_rados_cluster_stat_t</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
