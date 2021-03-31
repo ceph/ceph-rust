54fbd509afaf106b4917fc94bdac5640f29d9d3c
mzhong820 <Michelle_Zhong2@comcast.com>
2021-03-31 14:17:16 -0400

    add unsafe impl Send/Sync for IoCtx

--- src/ceph.rs
+++ src/ceph.rs
@@ -335,6 +335,10 @@ pub struct IoCtx {
     ioctx: rados_ioctx_t,
 }
 
+unsafe impl Send for IoCtx {}
+unsafe impl Sync for IoCtx {}
+
+
 impl Drop for IoCtx {
     fn drop(&mut self) {
         if !self.ioctx.is_null() {
