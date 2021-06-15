d3a09df391556ba5a21f94920498ca5a84ba536a
John Spray <john@putget.io>
2021-06-15 10:22:49 +0100

    async: add the basics (write/append/read/remove)

--- src/ceph.rs
+++ src/ceph.rs
@@ -26,6 +26,7 @@ use nom::number::complete::le_u32;
 use nom::IResult;
 use serde_json;
 
+use crate::completion::with_completion;
 use crate::rados::*;
 #[cfg(feature = "rados_striper")]
 use crate::rados_striper::*;
@@ -48,6 +49,8 @@ const CEPH_OSD_TMAP_HDR: char = 'h';
 const CEPH_OSD_TMAP_SET: char = 's';
 const CEPH_OSD_TMAP_CREATE: char = 'c';
 const CEPH_OSD_TMAP_RM: char = 'r';
+
+const DEFAULT_READ_BYTES: usize = 64 * 1024;
 
 #[derive(Debug, Clone)]
 pub enum CephHealth {
@@ -938,6 +941,119 @@ impl IoCtx {
         Ok(())
     }
 
+    pub async fn rados_async_object_write(
+        &self,
+        object_name: &str,
+        buffer: &[u8],
+        offset: u64,
+    ) -> RadosResult<u32> {
+        self.ioctx_guard()?;
+        let obj_name_str = CString::new(object_name)?;
+
+        with_completion(&self, |c| unsafe {
+            rados_aio_write(
+                self.ioctx,
+                obj_name_str.as_ptr(),
+                c,
+                buffer.as_ptr() as *const ::libc::c_char,
+                buffer.len(),
+                offset,
+            )
+        })?
+        .await
+    }
+
+    /// Async variant of rados_object_append
+    pub async fn rados_async_object_append(
+        self: &Arc<Self>,
+        object_name: &str,
+        buffer: &[u8],
+    ) -> RadosResult<u32> {
+        self.ioctx_guard()?;
+        let obj_name_str = CString::new(object_name)?;
+
+        with_completion(self, |c| unsafe {
+            rados_aio_append(
+                self.ioctx,
+                obj_name_str.as_ptr(),
+                c,
+                buffer.as_ptr() as *const ::libc::c_char,
+                buffer.len(),
+            )
+        })?
+        .await
+    }
+
+    /// Async variant of rados_object_write_full
+    pub async fn rados_async_object_write_full(
+        &self,
+        object_name: &str,
+        buffer: &[u8],
+    ) -> RadosResult<u32> {
+        self.ioctx_guard()?;
+        let obj_name_str = CString::new(object_name)?;
+
+        with_completion(&self, |c| unsafe {
+            rados_aio_write_full(
+                self.ioctx,
+                obj_name_str.as_ptr(),
+                c,
+                buffer.as_ptr() as *const ::libc::c_char,
+                buffer.len(),
+            )
+        })?
+        .await
+    }
+
+    /// Async variant of rados_object_remove
+    pub async fn rados_async_object_remove(&self, object_name: &str) -> RadosResult<()> {
+        self.ioctx_guard()?;
+        let object_name_str = CString::new(object_name)?;
+
+        with_completion(self, |c| unsafe {
+            rados_aio_remove(self.ioctx, object_name_str.as_ptr() as *const c_char, c)
+        })?
+        .await
+        .map(|_r| ())
+    }
+
+    /// Async variant of rados_object_read
+    pub async fn rados_async_object_read(
+        self: &Arc<Self>,
+        object_name: &str,
+        fill_buffer: &mut Vec<u8>,
+        read_offset: u64,
+    ) -> RadosResult<u32> {
+        self.ioctx_guard()?;
+        let obj_name_str = CString::new(object_name)?;
+
+        if fill_buffer.capacity() == 0 {
+            fill_buffer.reserve_exact(DEFAULT_READ_BYTES);
+        }
+
+        let result = with_completion(self, |c| unsafe {
+            rados_aio_read(
+                self.ioctx,
+                obj_name_str.as_ptr(),
+                c,
+                fill_buffer.as_mut_ptr() as *mut c_char,
+                fill_buffer.capacity(),
+                read_offset,
+            )
+        })?
+        .await;
+
+        if let Ok(rval) = &result {
+            unsafe {
+                let len = *rval as usize;
+                assert!(len <= fill_buffer.capacity());
+                fill_buffer.set_len(len);
+            }
+        }
+
+        result
+    }
+
     /// Efficiently copy a portion of one object to another
     /// If the underlying filesystem on the OSD supports it, this will be a
     /// copy-on-write clone.
@@ -995,7 +1111,7 @@ impl IoCtx {
     /// amount of bytes read
     /// The io context determines the snapshot to read from, if any was set by
     /// rados_ioctx_snap_set_read().
-    /// Default read size is 64K unless you call Vec::with_capacity(1024*128)
+    /// Default read size is 64K unless you call Vec::with_capacity
     /// with a larger size.
     pub fn rados_object_read(
         &self,
@@ -1007,7 +1123,7 @@ impl IoCtx {
         let object_name_str = CString::new(object_name)?;
         let mut len = fill_buffer.capacity();
         if len == 0 {
-            fill_buffer.reserve_exact(1024 * 64);
+            fill_buffer.reserve_exact(DEFAULT_READ_BYTES);
             len = fill_buffer.capacity();
         }
 
